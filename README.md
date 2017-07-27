7b0ad506f04fa873babd0653b82628ffbc507512
Kostja <kostja@acrosslimits.com>
2017-07-27 13:04:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# sql-stringifier
+
+sql-stringifier was made to allow you make fast and secure sql requests.
+
+## Usage examples
+
+### Select data
+
+```js
+const sqlStringifier = require('sql-stringifier');
+
+sqlStringifier.stringify({
+    table: 'users',
+    select: ['name', 'age'],
+    where: {
+        name: 'John',
+        age: {
+            gt: 18
+        }
+    }
+});
+
+// SELECT name,age FROM `users` WHERE `name` = 'John' AND `age` <= 18;
+```
+
+### Insert data
+
+```js
+const sqlStringifier = require('sql-stringifier');
+
+sqlStringifier.stringify({
+    table: 'users',
+    insert: {
+        values: {
+            name: 'John',
+            age: 21
+        }
+    }
+});
+
+// INSERT INTO `users` (`name`, `age`) VALUES ('John', 21);
+```
+
+### Error handling
+
+```js
+const sqlStringifier = require('sql-stringifier');
+
+try {
+    sqlStringifier.stringify({
+        insert: {
+            values: {
+                name: 'John',
+                age: 21
+            }
+        }
+    });
+} catch (error) {
+    /* handle error */
+}
+```
+### API
+
+#### stringify(options)
+
+- `options` {Object}
+  - `where` {Object}
+  
+    - `gt` - selects those rows where the value of the field is greater than the specified value.
+    - `gte` - selects the rows where the value of the field is greater than or equal to a specified value.
+    - `lt` - selects the rows where the value of the field is less than the specified value.
+    - `lte` - selects the rows where the value of the field is less than or equal to the specified value.
+    - `like` - [SQL LIKE Operator](https://dev.mysql.com/doc/refman/5.7/en/pattern-matching.html)
+    - `regexp` - [SQL REGEXP Operator](https://dev.mysql.com/doc/refman/5.7/en/regexp.html)
---
+++ index.js
@@ -0,0 +1,140 @@
+'use strict';
+
+var SqlString = require('sqlstring');
+
+function print(value) {
+
+    switch (typeof value) {
+        case 'string':
+            return SqlString.escape(value);
+            break;
+        case 'number':
+            return value;
+            break;
+    }
+}
+
+var main = module.exports = {
+
+    stringify: function(object) {
+
+        var config = {};
+
+        if (typeof object !== 'object') return Error('First parameter should be an object');
+        if (typeof object.table !== 'string') return Error('Table is not defined');
+
+        if (object.select) {
+            config.result = 'SELECT ' + (typeof object.select === 'object' ? object.select.join(',') : (object.select ? object.select : '*')) + ' FROM `' + object.table + '`';
+            config.type = 'select';
+        } else if (object.insert) {
+            config.result = 'INSERT INTO `' + object.table + '`';
+            config.type = 'insert';
+        }
+
+        if (config.type === 'insert') {
+            var firstPart = '';
+            var secondPart = '';
+
+            for (var option in object.insert.values) {
+                firstPart += (firstPart ? ', ' : '') + '`' + option + '`';
+                secondPart += (secondPart ? ', ' : '') + print(object.insert.values[option]);
+            }
+            config.result += ' (' + firstPart + ') VALUES (' + secondPart + ')';
+        }
+
+        if (['select', 'update', 'delete'].indexOf(config.type) > -1 && object.where && typeof object.where === 'object') {
+
+            config.result += ' WHERE';
+
+            config.once = false;
+
+            for (var option in object.where) {
+
+                switch (typeof object.where[option]) {
+                    case 'string':
+                    case 'number':
+
+
+                        config.result += (config.once ? ' AND `' : ' `') + option + "` = " + print(object.where[option]);
+                        config.once = true;
+
+                        break;
+
+
+                    case 'object':
+
+                        if (object.where[option].lt) {
+                            switch (typeof object.where[option].lt) {
+                                case 'string':
+                                case 'number':
+
+                                    config.result += (config.once ? ' AND `' : ' `') + option + "` > " + print(object.where[option].lt);
+                                    config.once = true;
+                                    break;
+                            }
+                        } else if (object.where[option].lte) {
+                            switch (typeof object.where[option].lte) {
+                                case 'string':
+                                case 'number':
+
+                                    config.result += (config.once ? ' AND `' : ' `') + option + "` >= " + print(object.where[option].lte);
+                                    config.once = true;
+                                    break;
+                            }
+                        }
+
+                        if (object.where[option].gt) {
+                            switch (typeof object.where[option].gt) {
+                                case 'string':
+                                case 'number':
+
+                                    config.result += (config.once ? ' AND `' : ' `') + option + "` <= " + print(object.where[option].gt);
+                                    config.once = true;
+                                    break;
+                            }
+                        } else if (object.where[option].gte) {
+                            switch (typeof object.where[option].gte) {
+                                case 'string':
+                                case 'number':
+
+                                    config.result += (config.once ? ' AND `' : ' `') + option + "` <= " + print(object.where[option].gte);
+                                    config.once = true;
+                                    break;
+                            }
+                        }
+
+                        if (object.where[option].like) {
+                            if (typeof object.where[option].like === 'string') {
+
+
+
+                                config.result += (config.once ? ' AND `' : ' `') + option + "` LIKE " + print(object.where[option].like);
+                                config.once = true;
+
+                            }
+                        }
+
+                        if (object.where[option].regexp) {
+                            if (typeof object.where[option].regexp === 'string') {
+
+
+
+                                config.result += (config.once ? ' AND `' : ' `') + option + "` REGEXP " + print(object.where[option].regexp);
+                                config.once = true;
+
+                            }
+                        }
+
+
+
+                        break;
+                }
+
+            }
+
+        }
+
+        return config.result + `;`;
+
+    }
+};
